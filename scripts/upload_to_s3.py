4f2d3b7e90cfc32d38ee9566af3c50e2bf64cd55
namikimlab <namixkim@gmail.com>
2025-05-30 15:13:06 +0900

    Add comment

--- scripts/upload_to_s3.py
+++ scripts/upload_to_s3.py
@@ -1,3 +1,7 @@
+# This script uploads a local data file to an S3 bucket.
+# In production, you would typically extract data directly from the source,
+# and not upload local files like this. This is mainly for testing or initial load.
+
 import boto3
 import os
 from dotenv import load_dotenv
@@ -6,11 +10,13 @@ from botocore.exceptions import NoCredentialsError, ClientError
 # Load environment variables from .env
 load_dotenv()
 
+# Environment variables (should be set in .env file)
 BUCKET_NAME = os.getenv("S3_BUCKET")
-S3_KEY = os.getenv("S3_KEY")
+S3_KEY = os.getenv("S3_KEY") # S3 object key (i.e., path/filename in S3)
 LOCAL_FILE_PATH = os.getenv("LOCAL_FILE_PATH")
 
 def upload_to_s3():
+    """Uploads a local file to the specified S3 bucket."""
     try:
         s3 = boto3.client('s3')
         s3.upload_file(LOCAL_FILE_PATH, BUCKET_NAME, S3_KEY)
